cf101482131b322235c62599a3883712172bf94c
Janray <janraydev@gmail.com>
2026-07-16 22:47:25 +0800

    feat: add a rate limiter

--- src/feature/auth/auth.routes.ts
+++ src/feature/auth/auth.routes.ts
@@ -4,10 +4,21 @@ import { login, register } from "./auth.controller.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 import { validateRequest } from "../../middleware/validateRequest.js";
 import { loginRequest, registerRequest } from "./auth.schema.js";
+import { limiter } from "../../middleware/rateLimit.js";
 
 const router = Router();
 
-router.post("/register", validateRequest(registerRequest) , asyncHandler(register));
-router.post("/login", validateRequest(loginRequest), asyncHandler(login));
- 
+router.post(
+  "/register",
+  validateRequest(registerRequest),
+  asyncHandler(register),
+);
+
+router.post(
+  "/login",
+  limiter,
+  validateRequest(loginRequest),
+  asyncHandler(login),
+);
+
 export default router;
